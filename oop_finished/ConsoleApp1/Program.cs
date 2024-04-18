fc30679b3c78230b34d05a99ec3d668a4e9613c5
mohamed amin <maming7510@gmail.com>
2024-04-18 18:57:01 +0200

    first commit

---
+++ oop_finished/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+
+    abstract class superclass
+    {
+        public int number = 3;
+        public abstract void childclass1();
+
+        public abstract void childclass2();
+        
+        
+
+    }
+
+    class subclass1:superclass
+    {
+         int number = 3;
+        public  override void childclass1() 
+        {
+            Console.WriteLine("this is a childclass1 in subclass1");
+        }
+        public override  void childclass2() 
+        {
+            Console.WriteLine("this is a childclass1 in subclass1"); 
+        }
+
+
+    
+   
+    }
+
+
+
+
+
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+          
+            var x=new subclass1 ();
+            x.childclass1 ();
+            x.childclass2 ();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oop_finished/oop_finished/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+namespace  passwordmangement
+{
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //phone x=phone("01025656");
+            List<int>numbers=new List<int>();
+
+            foreach(var i in numbers) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
